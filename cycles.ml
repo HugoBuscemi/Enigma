9b9146112f9a994c18620405ddf01de8bc19c450
Hugo Buscemi <hBusc926@stp1-pc04.ensr.local>
2021-09-13 14:53:34 +0200

    Debut du comportement de ./cycles

--- cycles.ml
+++ cycles.ml
@@ -4,3 +4,9 @@ let graph_of_known_cipher a b =
     Graph.add_edge g (Symbol.of_char a.[i]) (Symbol.of_char b.[i]) i
   done;
   g;;
+
+
+let () =
+  if Filename.basename Sys.argv.(0) = "tests" then begin
+      let g = graph_of_known_cipher Sys.argv.(1) Sys.argv.(2) in ();
+    end
